0cc6174a2ecf809fb8700f59353a7accb9709916
David <dahercal29@gmail.com>
2021-09-24 20:05:31 -0600

    Inserts agregados

--- apiNode/src/repositories/database.ts
+++ apiNode/src/repositories/database.ts
@@ -3,12 +3,12 @@ import { mongodbSubastas } from './keys/keys'
 
 mongoose.connect(mongodbSubastas.URI2)
 .then(() =>{
-    console.log('connected to db')
+    console.log('connected to db David')
     console.log(mongoose.connection.readyState);
 })
 .catch((err: any)=>{
     mongoose.connect(mongodbSubastas.URI1).then(()=>{
-        console.log('connected to db')
+        console.log('connected to db Sebastian')
         console.log(mongoose.connection.readyState);
     }
     ).catch((err: any)=>{
